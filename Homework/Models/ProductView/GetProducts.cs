6665e9ee2b5ecb7904084b59507f4268a28975c7
leg200298 <pig60209@yahoo.com.tw>
2019-11-07 22:57:16 +0800

    建立ProductController和View的Product資料夾

---
+++ Homework/Models/ProductView/GetProducts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework.Models.ProductView
+{
+    public class GetProducts : PostProduct
+    {
+        public int ProductID { get; set; }
+    }
+}
